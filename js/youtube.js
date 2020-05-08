d23fd6c0e7a3b3af2030df1e6fa179bb806873c9
Alexander Falzberger <falzberger.alexander@gmail.com>
2020-05-08 23:48:08 +0200

    fixing and adding additional youtube player

--- js/youtube.js
+++ js/youtube.js
@@ -1,34 +1,43 @@
 // 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // calculate size of iframe
-let h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-h -= 100;
-let w = h * 16 / 9;
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+let w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+w -= 50;
+w = w > 900 ? 900 : w;
+let h = w * 9 / 16;
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
+let player1;
+let player2;
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: h,
-    width: w,
-    videoId: 's4Z97QVhb9k',
-    events: {
-      'onReady': onPlayerReady
-    }
-  });
+    player1 = new YT.Player('player1', {
+        height: h,
+        width: w,
+        videoId: 's4Z97QVhb9k',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+    player2 = new YT.Player('player2', {
+        height: h,
+        width: w,
+        videoId: '1ZgTbh3psRI',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
 }
 
 // 4. The API will call this function when the video player is ready.
-function onPlayerReady() {
-  player.loadPlaylist({
-    list: 'PLYiypSXRnsMrhMqELv9y1ukaCvphjy1vP'
-  });
-  // function for autoplay: event.target.playVideo();
+function onPlayerReady(event) {
+    // event.target.loadPlaylist({
+    //     list: 'PLYiypSXRnsMrhMqELv9y1ukaCvphjy1vP'
+    // });
+    // function for autoplay: event.target.playVideo();
 }
